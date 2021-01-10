bc1248a5ded61e3650d6f653c42846c104dd5a84
Margarita Morozova <ritamorozova@yahoo.com>
2021-01-10 15:36:37 -0800

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,53 @@
 // Code your solution here!
 
+function printString(myString){
+  console.log(myString[0]);
+  if (myString.length > 1){
+    let mySubString = myString.substring(1, myString.length)
+    printString(mySubString)
+  }else{
+    return true
+  }
+}
+
+function reverseString(myString){
+  if (myString.length < 2){
+    return myString
+  }else{
+    return reverseString(myString.substring(1))+myString[0]
+  }
+}
+
+function isPalindrome(myString){
+  let size = myString.length
+  if(size<2){
+    return true
+  }else if (myString[size-1] === myString[0]){
+    return isPalindrome(myString.substring(1, size-1))
+  }else{
+    return false
+  }
+}
+
+function addUpTo(array, index){
+  return index ? array[index] + addUpTo(array, -- index) : array[index]
+}
+
+function maxOf(array){
+  if(array.length === 1){
+    return array[0]
+  }else{
+    return Math.max(array.pop(), maxOf(array))
+  }
+}
+
+function includesNumber(array, num){
+  if(!array.length){
+    return false
+  }else if (array[0] === num){
+    return true
+  }else{
+    return includesNumber(array.slice(1), num)
+  }
+
+}
